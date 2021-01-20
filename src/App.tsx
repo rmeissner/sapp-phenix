28148fef3d686342220b10d94d15dea8f51fb94e
Richard Meissner <msc.meissner@gmail.com>
2021-01-20 01:58:47 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { Button, Loader, Title } from '@gnosis.pm/safe-react-components';
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
+import { ethers } from 'ethers';
+import DelayedTxModule from './contracts/DelayedTxModule.json'
+import { SafeAppsSdkProvider } from './safe/provider'
 
 const Container = styled.form`
   margin-bottom: 2rem;
@@ -17,6 +20,24 @@ const Container = styled.form`
 const App: React.FC = () => {
   const { sdk, safe } = useSafeAppsSDK();
   const [submitting, setSubmitting] = useState(false);
+  const [moduleName, setModuleName] = useState("");
+  const module = useMemo(() => {
+    console.log("create module")
+    return new ethers.Contract(DelayedTxModule.address, DelayedTxModule.abi, new SafeAppsSdkProvider(safe, sdk));
+  }, [sdk, safe])
+
+  useEffect(() => {
+    console.log("load module name")
+    const loader = async () => {
+      try {
+        setModuleName(await module.NAME())
+      } catch (e) {
+        console.error(e)
+        setModuleName("errored")
+      }
+    };
+    loader();
+  }, [module])
 
   const submitTx = useCallback(async () => {
     setSubmitting(true);
@@ -41,6 +62,7 @@ const App: React.FC = () => {
 
   return (
     <Container>
+      <Title size="md">Interact with {moduleName}</Title>
       <Title size="md">{safe.safeAddress}</Title>
       {submitting ? (
         <>
