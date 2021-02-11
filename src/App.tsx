364447c61c6b2d1d86645f15011255e7c654b6b2
Mikhail <mmvsha73@gmail.com>
2021-02-11 17:51:30 +0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useMemo, useState } from 'react';
-import styled from 'styled-components';
-import { Loader } from '@gnosis.pm/safe-react-components';
-import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
-import { ethers } from 'ethers';
-import DelayedTxModule from './contracts/DelayedTxModule.json'
-import Safe from './contracts/Safe1_1_1.json'
-import { SafeAppsSdkProvider } from '@gnosis.pm/safe-apps-ethers-provider'
-import Rebirth from './components/rebirth';
-import Dashboard from './components/dashboard';
+import React, { useMemo, useState } from "react"
+import styled from "styled-components"
+import { Loader } from "@gnosis.pm/safe-react-components"
+import { useSafeAppsSDK } from "@gnosis.pm/safe-apps-react-sdk"
+import { ethers } from "ethers"
+import DelayedTxModule from "./contracts/DelayedTxModule.json"
+import Safe from "./contracts/Safe1_1_1.json"
+import { SafeAppsSdkSigner } from "@gnosis.pm/safe-apps-ethers-provider"
+import Rebirth from "./components/rebirth"
+import Dashboard from "./components/dashboard"
 
 const Container = styled.form`
   margin-bottom: 2rem;
@@ -18,20 +18,28 @@ const Container = styled.form`
   grid-template-columns: 1fr;
   grid-column-gap: 1rem;
   grid-row-gap: 1rem;
-`;
+`
 
 const App: React.FC = () => {
-  const { sdk, safe } = useSafeAppsSDK();
-  const [selectedTab, setSelectedTab] = useState("dashboard");
+  const { sdk, safe } = useSafeAppsSDK()
+  const [selectedTab, setSelectedTab] = useState("dashboard")
   const module = useMemo(() => {
     console.log("create module")
-    return new ethers.Contract(DelayedTxModule.address, DelayedTxModule.abi, new SafeAppsSdkProvider(safe, sdk));
+    return new ethers.Contract(
+      DelayedTxModule.address,
+      DelayedTxModule.abi,
+      new SafeAppsSdkSigner(safe, sdk)
+    )
   }, [sdk, safe])
 
   const manager = useMemo(() => {
     console.log("create manager " + safe.safeAddress)
     if (!safe.safeAddress) return undefined
-    return new ethers.Contract(safe.safeAddress, Safe.abi, new SafeAppsSdkProvider(safe, sdk));
+    return new ethers.Contract(
+      safe.safeAddress,
+      Safe.abi,
+      new SafeAppsSdkSigner(safe, sdk)
+    )
   }, [sdk, safe])
 
   const section = useMemo(() => {
@@ -45,16 +53,21 @@ const App: React.FC = () => {
     }
   }, [selectedTab, module, manager])
 
-  if (!manager) return (
-    <Loader size="md" />
-  )
+  if (!manager) return <Loader size="md" />
 
   return (
     <Container>
-      <div><a href="/#" onClick={() => setSelectedTab("dashboard")}>Dashboard</a> <a href="/#" onClick={() => setSelectedTab("rebirth")}>Rebirth</a></div>
+      <div>
+        <a href="/#" onClick={() => setSelectedTab("dashboard")}>
+          Dashboard
+        </a>{" "}
+        <a href="/#" onClick={() => setSelectedTab("rebirth")}>
+          Rebirth
+        </a>
+      </div>
       {section}
     </Container>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,23 +1,26 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Button, Title } from '@gnosis.pm/safe-react-components';
-import { Contract } from "@ethersproject/contracts";
-import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
-import Rescuers from './rescuers';
+import React, { useCallback, useEffect, useMemo, useState } from "react"
+import { Button, Title } from "@gnosis.pm/safe-react-components"
+import { Contract } from "@ethersproject/contracts"
+import { useSafeAppsSDK } from "@gnosis.pm/safe-apps-react-sdk"
+import Rescuers from "./rescuers"
 
 interface Props {
-  manager: Contract,
+  manager: Contract
   module: Contract
 }
 
 interface Status {
-  label: string,
+  label: string
   showEnableButton: boolean
 }
 
 const Dashboard: React.FC<Props> = ({ module, manager }) => {
-  const { sdk, safe } = useSafeAppsSDK();
-  const [moduleStatus, setModuleStatus] = useState({ label: "loading...", showEnableButton: false });
-  const [moduleName, setModuleName] = useState("loading...");
+  const { sdk, safe } = useSafeAppsSDK()
+  const [moduleStatus, setModuleStatus] = useState({
+    label: "loading...",
+    showEnableButton: false,
+  })
+  const [moduleName, setModuleName] = useState("loading...")
 
   useEffect(() => {
     const loadName = async () => {
@@ -27,47 +30,50 @@ const Dashboard: React.FC<Props> = ({ module, manager }) => {
         console.error(e)
         setModuleName("errored")
       }
-    };
-    loadName();
+    }
+    loadName()
     const loadEnabled = async () => {
       try {
         const modules = await manager.getModules()
         console.log(modules)
         console.log(module.address)
         const enabled = modules.includes(module.address)
-        setModuleStatus({ label: enabled ? "enabled" : "disabled", showEnableButton: !enabled } )
+        setModuleStatus({
+          label: enabled ? "enabled" : "disabled",
+          showEnableButton: !enabled,
+        })
       } catch (e) {
         console.error(e)
         setModuleStatus({ label: "errored", showEnableButton: false })
       }
-    };
-    loadEnabled();
+    }
+    loadEnabled()
   }, [manager, module])
 
   const enableModule = useCallback(async () => {
+    console.log({ addr: module.address })
     try {
-        await sdk.txs.send({
-            txs: [
-                {
-                    to: safe.safeAddress,
-                    value: '0',
-                    data: manager.interface.encodeFunctionData("enableModule", [module.address])
-                },
-            ],
-        });
+      await manager.enableModule(module.address)
     } catch (e) {
-        console.error(e);
+      console.error(e)
     }
-}, [safe, sdk]);
+  }, [safe, sdk])
 
-  return (<>
-    <Title size="md">Dashboard</Title>
-    <div>Name: {moduleName}</div>
-    <div>Status: {moduleStatus.label} {!moduleStatus.showEnableButton || <Button size="md" color="primary" onClick={enableModule}>Enable</Button>}</div>
-    <Rescuers module={module} enabled={true} />
-
+  return (
+    <>
+      <Title size="md">Dashboard</Title>
+      <div>Name: {moduleName}</div>
+      <div>
+        Status: {moduleStatus.label}{" "}
+        {!moduleStatus.showEnableButton || (
+          <Button size="md" color="primary" onClick={enableModule}>
+            Enable
+          </Button>
+        )}
+      </div>
+      <Rescuers module={module} enabled={true} />
     </>
   )
 }
 
-export default Dashboard;
+export default Dashboard
