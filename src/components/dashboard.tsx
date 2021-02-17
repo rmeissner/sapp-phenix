ce6256016c01d6d5014973abf8bf540724e1cbdb
Mikhail Mikheev <mmvsha73@gmail.com>
2021-02-17 17:11:42 +0400

    Feature: Use ethers signer (#2)
    * use ethers.js signer
    
    * hook dependencyfix
    
    * signer
    
    * add yalc.lock to gitignore
    
    * dep bump
    
    * use beta ethers provider
    
    * remove console.log
    
    * build fix

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,73 +1,65 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Button, Title } from '@gnosis.pm/safe-react-components';
-import { Contract } from "@ethersproject/contracts";
-import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
+import { Contract } from '@ethersproject/contracts';
 import Rescuers from './rescuers';
 
 interface Props {
-  manager: Contract,
-  module: Contract
-}
-
-interface Status {
-  label: string,
-  showEnableButton: boolean
+  manager: Contract;
+  module: Contract;
 }
 
 const Dashboard: React.FC<Props> = ({ module, manager }) => {
-  const { sdk, safe } = useSafeAppsSDK();
-  const [moduleStatus, setModuleStatus] = useState({ label: "loading...", showEnableButton: false });
-  const [moduleName, setModuleName] = useState("loading...");
+  const [moduleStatus, setModuleStatus] = useState({ label: 'loading...', showEnableButton: false });
+  const [moduleName, setModuleName] = useState('loading...');
 
   useEffect(() => {
     const loadName = async () => {
       try {
-        setModuleName(await module.NAME())
+        setModuleName(await module.NAME());
       } catch (e) {
-        console.error(e)
-        setModuleName("errored")
+        console.error(e);
+        setModuleName('errored');
       }
     };
     loadName();
     const loadEnabled = async () => {
       try {
-        const modules = await manager.getModules()
-        console.log(modules)
-        console.log(module.address)
-        const enabled = modules.includes(module.address)
-        setModuleStatus({ label: enabled ? "enabled" : "disabled", showEnableButton: !enabled } )
+        const modules = await manager.getModules();
+        console.log(modules);
+        console.log(module.address);
+        const enabled = modules.includes(module.address);
+        setModuleStatus({ label: enabled ? 'enabled' : 'disabled', showEnableButton: !enabled });
       } catch (e) {
-        console.error(e)
-        setModuleStatus({ label: "errored", showEnableButton: false })
+        console.error(e);
+        setModuleStatus({ label: 'errored', showEnableButton: false });
       }
     };
     loadEnabled();
-  }, [manager, module])
+  }, [manager, module]);
 
   const enableModule = useCallback(async () => {
     try {
-        await sdk.txs.send({
-            txs: [
-                {
-                    to: safe.safeAddress,
-                    value: '0',
-                    data: manager.interface.encodeFunctionData("enableModule", [module.address])
-                },
-            ],
-        });
+      await manager.enableModule(module.address);
     } catch (e) {
-        console.error(e);
+      console.error(e);
     }
-}, [safe, sdk]);
+  }, [manager, module.address]);
 
-  return (<>
-    <Title size="md">Dashboard</Title>
-    <div>Name: {moduleName}</div>
-    <div>Status: {moduleStatus.label} {!moduleStatus.showEnableButton || <Button size="md" color="primary" onClick={enableModule}>Enable</Button>}</div>
-    <Rescuers module={module} enabled={true} />
-
+  return (
+    <>
+      <Title size="md">Dashboard</Title>
+      <div>Name: {moduleName}</div>
+      <div>
+        Status: {moduleStatus.label}{' '}
+        {!moduleStatus.showEnableButton || (
+          <Button size="md" color="primary" onClick={enableModule}>
+            Enable
+          </Button>
+        )}
+      </div>
+      <Rescuers module={module} enabled={true} />
     </>
-  )
-}
+  );
+};
 
 export default Dashboard;
